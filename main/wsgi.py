05abd97f11620daa376ed1f88b4f2272304d86de
hikeolin <hikenike6@gmail.com>
2021-01-27 10:12:46 +0100

    change config directories

--- main/wsgi.py
+++ main/wsgi.py
@@ -14,7 +14,7 @@ from django.core.wsgi import get_wsgi_application
 env = os.environ.get("DJANGO_APP_ENV")
 if env is not None:
     if 'dev' in env.lower():
-        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.config.dev')
     else:
         os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.config.production')
 else:
